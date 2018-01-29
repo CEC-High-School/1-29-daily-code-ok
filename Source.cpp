93de7185c12c10a1f371f48c9be15392be1b1009
Dark Stallion <gonzalez.ferny31@outlook.com>
2018-01-29 07:57:24 -0700

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+int main() {
+	double radius;
+	double area;
+	double circumference;
+	while (true) {
+		cout << "What is the radius of the circle?" << endl;
+		cin >> radius;
+		area = (radius * radius)* 3.14;
+		circumference = (radius*3.14) * 2;
+		cout << "The area of the circle is " << area << endl;
+		cout << "The circumference of the circle is " << circumference << endl;
 
-//instructions for 1-19
-//
-//intro: Write a program that asks a user for how old they are, and then repeats that number back to them in an insult.
-//
-//intermediate: Write a program that prints the numbers 256 down to 32, counting by fours
-//
-//advanced: make an account at HackerRank.com
-//do this problem: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+	}
+	return 0;
+
+}
